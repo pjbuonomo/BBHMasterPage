a7fa7d9fc1d75a3c93abcc9b80bbd025a348f11d
thezigzag <pjbuonomo@me.com>
2023-05-09 15:21:50 -0400

    Update requests-count.js

--- requests-count.js
+++ requests-count.js
@@ -1,7 +1,21 @@
-function getAverageCompletionTime() {
+function getAverageCompletionTime(range) {
     var siteUrl = _spPageContextInfo.webAbsoluteUrl;
     var requestUri = siteUrl + "/_api/web/lists/getbytitle('PurchaseRequests')/items?$select=Assigned_x0020_Date,Completed_x0020_Date";
     
+    var now = new Date();
+    var startDate;
+    switch (range) {
+      case "week":
+        startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+        break;
+      case "month":
+        startDate = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        break;
+      case "year":
+        startDate = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
+        break;
+    }
+  
     $.ajax({
       url: requestUri,
       type: "GET",
@@ -12,6 +26,8 @@ function getAverageCompletionTime() {
         var results = data.d.results;
         var totalTime = 0;
         var validItems = 0;
+        var previousTotalTime = 0;
+        var previousValidItems = 0;
   
         for (var i = 0; i < results.length; i++) {
           var createdDate = new Date(results[i].Assigned_x0020_Date);
@@ -20,12 +36,21 @@ function getAverageCompletionTime() {
           if (completedDate) {
             completedDate = new Date(completedDate);
             var timeDifference = completedDate - createdDate;
-            totalTime += timeDifference;
-            validItems++;
+            if (createdDate >= startDate) {
+              totalTime += timeDifference;
+              validItems++;
+            } else {
+              previousTotalTime += timeDifference;
+              previousValidItems++;
+            }
           }
         }
   
         var averageCompletionTime = totalTime / validItems;
+        var previousAverageCompletionTime = previousTotalTime / previousValidItems;
+  
+        // Calculate the percentage change
+        var percentageChange = ((averageCompletionTime - previousAverageCompletionTime) / previousAverageCompletionTime) * 100;
   
         // Convert milliseconds to a more readable format (e.g., days, hours, minutes)
         var minutes = Math.floor(averageCompletionTime / 60000);
@@ -38,7 +63,8 @@ function getAverageCompletionTime() {
         var formattedTime = days + " d " + hours + " hr";
   
         // Update the inner HTML of the element with the specified ID
-        $('#AverageCompletionTime').html(formattedTime);
+        $('#newRequests').html(formattedTime);
+        $('#percentage-trend').html((percentageChange > 0 ? '+' : '') + percentageChange.toFixed(2) + '%');
       },
       error: function (error) {
         console.log("Error:", error);
@@ -47,5 +73,28 @@ function getAverageCompletionTime() {
   }
   
   $(document).ready(function () {
-    getAverageCompletionTime();
+    getAverageCompletionTime('week');
+    
+    $('a.dropdown-item').on('click', function (e) {
+      e.preventDefault();
+      var range = $(this).text().trim().toLowerCase().replace('this ', '');
+      getAverageCompletionTime(range);
+    });
   });
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
